a920428b5e35d48feec5c7b3915f9eb6a5b6a665
Yu Xuan <ngyuxuan1997@gmail.com>
2024-06-24 22:15:24 +0800

    Initial Commit

---
+++ Appetizers/AppetizersApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppetizersApp.swift
+//  Appetizers
+//
+//  Created by Yu Xuan Ng on 24/6/24.
+//
+
+import SwiftUI
+
+@main
+struct AppetizersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
